e816646b268a0bc2ea5338341d5880c74e05a6ff
Your Name <mdsultanmahmud.bd00@gmail.com>
2025-01-18 12:22:24 +0600

    feat: added home services section

---
+++ assets/js/servicesSlider.js
@@ -0,0 +1,28 @@
+const swiper = new Swiper(".seba__services__container", {
+  loop: true,
+  autoplay: {
+    delay: 3000,
+  },
+  //   pagination: {
+  //     el: '.swiper-pagination',
+  //     clickable: true,
+  //   },
+  breakpoints: {
+    640: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 5,
+      spaceBetween: 50,
+    },
+  },
+  navigation: {
+    nextEl: ".servcies__next__btn",
+    prevEl: ".services__prev__btn",
+  },
+});
